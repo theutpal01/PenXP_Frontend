9f73974ec9730290cb6c1aa0e2d38c7c99e53399
Utpal <dev.theutpal@gmail.com>
2025-02-03 23:29:25 +0530

    FIX: made some changes

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
 
 function Hero() {
 	return (
 		<div className="bg-gradient-to-br from-purple-400 via-pink-500 to-yellow-300 w-full h-screen flex items-center justify-center">
+			<img src="/cd.png" alt="" />
 			<h1 className="text-8xl text-white font-medium animate-pulse">WELCOME TO PENXP</h1>
 		</div>
 	)
